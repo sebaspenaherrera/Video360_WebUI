1f72c82fc63bb34e31858ab770ad0151b75655ee
sebaspenaherrera <sebasspenaherrera@gmail.com>
2024-05-27 16:34:34 +0200

    Commit 14
    Port flag fixed

--- run-api.sh
+++ run-api.sh
@@ -2,5 +2,8 @@
 
 . /opt/miniconda/bin/activate
 conda activate restapi
-python opt/Video360_WebUI/main.py -port 33334 > log-main.txt &
-python opt/Video360_WebUI/restapi.py -port 33333 > log-rest.txt &
+#python opt/Video360_WebUI/main.py --port 33334 > /logs/log-main.txt &
+#python opt/Video360_WebUI/restapi.py --port 33333 > /logs/log-rest.txt &
+
+python opt/Video360_WebUI/main.py --port 33334 &
+python opt/Video360_WebUI/restapi.py --port 33333 &
