c07c01ab5e00bfc68fb60c4669b707422f9d808e
sebaspenaherrera <sebasspenaherrera@gmail.com>
2024-05-30 12:21:25 +0200

    Commit 18
    Run ssh daemon through bash file instead of CMD in dockerfile. BASH file renamed to run.sh

---
+++ run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Activate the conda environment for the main screen
+#. /opt/miniconda/bin/activate
+#conda activate restapi
+
+/usr/sbin/sshd -D
+
+# Create a new tmux session for the API and WebUI
+#tmux new-session -s api -d
+#tmux new-session -s web -d
+
+# Initialize the API
+#tmux send-keys -t api ". /opt/miniconda/bin/activate restapi" 'Enter' 
+#tmux send-keys -t api "python /opt/Video360_WebUI/restapi.py --port 33333" 'Enter'
+
+# Initialize the WebUI
+#tmux send-keys -t web ". /opt/miniconda/bin/activate restapi" 'Enter' 
+#tmux send-keys -t web "python /opt/Video360_WebUI/main.py --port 33334" 'Enter'
